c5193c485c57f2c059436c8356ad622a739286ac
eggman <skouhei@gmail.com>
2016-06-25 10:08:23 +0900

    fix hello world

--- src/main.c
+++ src/main.c
@@ -5,7 +5,7 @@ int main(void)
     int i=0;
 
     while (1) {
-        DiagPrintf("Hello World : %d %d\r\n", i++);
+        DiagPrintf("Hello World : %d\r\n", i++);
         HalDelayUs(1000000);
     }
 }
